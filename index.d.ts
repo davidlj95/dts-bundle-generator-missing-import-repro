373e2f1ef7c73a53a2db73fce6ce5611fc1ef50e
David LJ <mail@davidlj95.com>
2024-04-19 12:46:33 +0200

    Initial commit with bug repro

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { ModuleWithProviders } from '@angular/core';
+import * as i0 from "@angular/core";
+
+export declare class SampleModule {
+    static forRoot(): ModuleWithProviders<unknown>;
+    static ɵfac: i0.ɵɵFactoryDeclaration<unknown, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<unknown, never, never, never>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<unknown>;
+}
